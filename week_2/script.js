25489d7ebc66056079d31e7f939cec18f0929662
LAB316 Student21 <LAB316_Student21@gau.local>
2019-09-27 15:07:15 +0400

    first commit in week_2

---
+++ week_2/script.js
@@ -0,0 +1,19 @@
+function testJsConsole(){
+    console.log("Hello JS, I am console");
+}
+
+function testJsAlert(){
+    alert("Hello JS, I am alert");
+}
+
+
+
+function sumOfTwoNumber(a,b){
+    console.log(a+b);
+}
+
+function divTextInput(t){
+    result = document.getElementById("result");
+    result.innerText = t;
+    
+}
